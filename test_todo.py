b687026f988cd4b1544fe204eb509a0086e46672
seanflaherty <sean.flaherty@gmail.com>
2024-12-09 19:00:27 -0500

    initial commit

---
+++ test_todo.py
@@ -0,0 +1,41 @@
+import todo
+
+def test_create_todo():
+    todo.todos = []
+    todo.create_todo(todo.todos,
+                     title="Make some stuff",
+                     description="Stuff needs to be programmed",
+                     level="Important")
+    
+    assert len(todo.todos) == 1, "Todo was not created!"
+    assert todo.todos[0]['title'] =="Make some stuff"
+    assert (todo.todos[0]['description'] ==
+            "Stuff needs to be programmed")
+    assert (todo.todos[0]['level']) == "Important"
+
+    print("ok - create_todo")
+
+def test_get_function():
+    assert todo.get_function('new') == todo.create_todo
+    print("ok - get_function")
+
+def test_get_fields():
+    assert (todo.get_fields('new') ==
+            ['title', 'description', 'level'])
+    print("ok - test_get_fields")   
+
+def test_run_command():
+    result = todo.run_command(
+        'test',
+        {'abcd':'efgh', 'ijkl':'mnop'}
+    )
+    expected = """Command 'test' returned:
+abcd: efgh
+ijkl: mnop"""
+    assert result == expected, \
+                result + " != " + expected
+    print ("ok - run_command")
+
+test_get_function()
+test_create_todo()
+test_run_command()
---
+++ todo.py
@@ -0,0 +1,56 @@
+def create_todo(todos, title, description, level):
+    todo = {
+        'title' : title,
+        'description' : description,
+        'level' : level
+    }
+    todos.append(todo)
+
+def main_loop():
+    user_input = ""
+    while 1:
+        print(run_command(user_input))
+        user_input = input("> ")
+        if user_input.lower().startswith("quit"):
+            print("Exiting...")
+            break
+
+def get_input(fields):
+    user_input = {}
+    for field in fields:
+        user_input[field] = input(field + " > ")
+    return user_input
+
+def test(todos, abcd, ijkl):
+    return "Command 'test' returned:\n" + \
+        "abcd: " + abcd + "\nijkl: " + ijkl
+
+#SCF
+commands = {
+    'new' : [create_todo,['title', 'description', 'level']],
+    'test' : [test, ['abcd', 'ijkl']],
+}
+
+todos = []
+
+def run_command(user_input, data=None):
+    user_input = user_input.lower()
+    if user_input not in commands:
+        return user_input + "?" \
+                " I don't know what that command is."
+    else:
+        the_func = get_function(user_input)
+
+    if data is None:
+        the_fields = get_fields(user_input)
+        data = get_input(the_fields)
+    return the_func(todos, **data)
+
+def get_function(command_name):
+    return commands[command_name][0]
+
+def get_fields(command_name):
+    return commands[command_name][1]
+
+if __name__ == '__main__':
+    main_loop()
