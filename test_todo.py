6c23afb40eef831df6b3b27dc219cd93e5dc6cea
seanflaherty <sean.flaherty@gmail.com>
2024-12-15 16:42:35 -0500

    Added code to load and save todos.

--- test_todo.py
+++ test_todo.py
@@ -1,4 +1,5 @@
 import todo
+import os
 
 def test_create_todo():
     todo.todos = []
@@ -36,6 +37,104 @@ ijkl: mnop"""
                 result + " != " + expected
     print ("ok - run_command")
 
+def test_show_todos():
+    todo.todos = [
+        { 'title' : 'test todo',
+          'description' : 'This is a test',
+          'level' : 'IMPORTANT'
+        }
+    ]
+    result = todo.show_todos(todo.todos)
+    lines = result.split("\n")
+
+    first_line = lines[0]
+    assert "Item" in first_line
+    assert "Title" in first_line
+    assert "Description" in first_line
+    assert "Level" in first_line
+    second_line = lines[1]
+    assert "1" in second_line
+    assert "test todo" in second_line
+    assert "This is a test" in second_line
+    assert "IMPORTANT" in second_line
+
+    print ("ok - show_todos")
+
+def test_todo_sort_order():
+    todo.todos = [
+        { 'title' : 'test unimportant todo',
+            'description' : 'An unimportant test',
+            'level' : 'Unimportant'
+        },
+        { 'title' : 'test medium todo',
+            'description' : 'A test',
+            'level' : 'Medium'
+        },
+        { 'title' : 'test important todo',
+            'description' : 'An important test',
+            'level' : 'Important'
+        },
+    ]
+    result = todo.show_todos(todo.todos)
+    lines = result.split("\n")
+    assert "IMPORTANT" in lines[1]
+    assert "Medium," in lines[2]
+    assert "Unimportant" in lines[3]
+
+    print("OK - todo sort order")
+
+def test_todo_wrap_long_lines():
+    todo.todos = [
+        { 'title' : 'test important todo',
+         'description' : ('This is an important '
+                          'test. We\'d really like '
+                          'this line to wrap '
+                          'several times, to '
+                          'imitate what might '
+                          'happen in a real '
+                          'program.'),
+                          'level' : 'IMPORTANT'
+                          },
+    ]
+    result = todo.show_todos(todo.todos)
+    lines = result.split("\n")
+
+    assert "test important" in lines[1]
+    assert "This is an important" in lines[1]
+
+    assert "todo" in lines[2]
+    assert "test. We'd really like" in lines[2]
+
+    assert "this line to wrap" in lines[3]
+    assert "several times, to" in lines[4]
+    assert "imitate what might" in lines[5]
+    assert "happen in a real" in lines[6]
+    assert "program." in lines[7]
+
+    print("ok - todo wrap long lines")
+
+def test_save_todo_list():
+            todos_original = [
+                 { 'title' : 'test todo',
+                  'description' : 'This is a test',
+                  'level' : 'IMPORTANT'
+                  }
+            ]
+            todo.todos = todos_original
+            assert "todos.pickle" in os.listdir('.')
+
+            todo.save_todo_list()
+            assert "todos.pickle" in os.listdir('.')
+
+            todo.load_todo_list()
+            assert todo.todos == todos_original
+            os.unlink("todos.pickle")
+            print("ok - save todo list")
+
 test_get_function()
 test_create_todo()
 test_run_command()
+test_get_fields()
+test_show_todos()
+test_todo_wrap_long_lines()
+test_save_todo_list()
--- todo.py
+++ todo.py
@@ -1,3 +1,7 @@
+import os
+import pickle
+import textwrap
+
 def create_todo(todos, title, description, level):
     todo = {
         'title' : title,
@@ -25,12 +29,6 @@ def test(todos, abcd, ijkl):
     return "Command 'test' returned:\n" + \
         "abcd: " + abcd + "\nijkl: " + ijkl
 
-#SCF
-commands = {
-    'new' : [create_todo,['title', 'description', 'level']],
-    'test' : [test, ['abcd', 'ijkl']],
-}
-
 todos = []
 
 def run_command(user_input, data=None):
@@ -52,5 +50,82 @@ def get_function(command_name):
 def get_fields(command_name):
     return commands[command_name][1]
 
+def capitalize(todo):
+    todo['level'] = todo['level'].upper()
+    return todo
+
+def save_todo_list():
+    save_file = open("todos.pickle", "wb")
+    pickle.dump(todos, save_file)
+    save_file.close()
+
+def load_todo_list():
+    global todos
+    if os.access("todos.pickle", os.F_OK):
+        #scf
+        save_file = open("todos.pickle", "rb")
+        todos = pickle.load(save_file)
+
+
+def show_todo(todo, index):
+    wrapped_title = textwrap.wrap(todo['title'], 16)
+    wrapped_descr = textwrap.wrap(todo['description'], 24)
+    output = str(index+1).ljust(8) + " "
+    output += wrapped_title[0].ljust(16) + " "
+    output += wrapped_descr[0].ljust(24) + " "
+    output += todo['level'].ljust(16)
+    output += "\n"
+
+    max_len = max(len(wrapped_title),
+                len(wrapped_descr))
+    for index in range(1, max_len):
+        output += " " * 8 + " "
+        if index < len(wrapped_title):
+            output += wrapped_title[index].ljust(16) + " "
+        else:
+            output += " " * 16 + " "
+        if index < len(wrapped_descr):
+            output += wrapped_descr[index].ljust(24) + " "
+        else:
+            output += " " * 24 + " "
+        output += "\n"
+    return output
+
+def show_todos(todos):
+    output = ("Item     Title           "
+              "Description              Level\n")
+    sorted_todos = sort_todos(todos)
+    
+    for index, todo in enumerate(sorted_todos):
+        output += show_todo(todo, index)
+    return output
+
+def sort_todos(todos):
+    important = [capitalize(todo) for todo in todos
+                if todo['level'].lower() == 'important']
+    unimportant = [todo for todo in todos
+                if todo['level'].lower() == 'unimportant']
+    medium = [todo for todo in todos
+                if todo['level'].lower() != 'important' and
+                    todo['level'].lower() != 'unimportant']
+    todos = important + medium + unimportant
+    return todos
+
+
+commands = {
+    'new' : [create_todo,['title', 'description', 'level']],
+    'show' : [show_todos, []],
+    'test' : [test, ['abcd', 'ijkl']],
+}
+
+
 if __name__ == '__main__':
+    user_input = ""
+    load_todo_list()
+    while 1:
+        print(run_command(user_input))
+        user_input = input("> ")
+        if user_input .lower().startswith("quit"):
+            print("Exiting...")
+            break
     main_loop()
